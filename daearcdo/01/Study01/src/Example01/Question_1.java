c7778664702d1cc626c6c620404c6729e9d7d1fd
kimkitae <daearcdo@gmail.com>
2015-09-30 19:40:25 +0900

    처음 실

---
+++ daearcdo/01/Study01/src/Example01/Question_1.java
@@ -0,0 +1,19 @@
+package Example01;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Sum = 0;
+
+		for (int i = 1; i <= 9; i++) {
+			for (int h = 1; h <= i; h++) {
+				Sum += h;
+				System.out.println("더한 수" + Sum);
+			}
+		}
+
+	}
+
+}
